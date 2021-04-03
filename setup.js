0e6227e1ef89bb3d76b0925eb4477ae6485f51c5
SilasAlb <81107877+SilasAlb@users.noreply.github.com>
2021-04-03 12:59:43 -0300

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,16 @@
+$ ( '.owl-carrossel' ) . owlCarousel ( {
+    loop : verdadeiro ,
+    margem : 10 ,
+    nav : false ,
+    responsivo : {
+        0 : {
+            itens : 1
+        } ,
+        600 : {
+            itens : 3
+        } ,
+        1000 : {
+            itens : 5
+        }
+    }
+} )
